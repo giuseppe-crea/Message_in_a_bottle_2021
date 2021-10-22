1a94751711d5fcfefcb895be20b8b2328a754c64
giuseppe-crea <7926745+Sylt556@users.noreply.github.com>
2021-10-22 12:38:07 +0200

    Removed the homebrew registration function, integrated the default one

--- monolith/views/auth.py
+++ monolith/views/auth.py
@@ -20,25 +20,6 @@ def login():
     return render_template('login.html', form=form)
 
 
-@auth.route('/register', methods=['GET', 'POST'])
-def register():
-    form = UserForm()
-    if form.validate_on_submit():
-        email, password, first_name, last_name, date_of_birth = \
-            form.data['email'], form.data['password'], form.data['firstname'], form.data['lastname'], form.data['dateofbirth']
-        q = db.session.query(User).filter(User.email == email)
-        user = q.first()
-        if user is not None:
-            return redirect('/register')
-        else:
-            me = User()
-            me.register_new_user(email, first_name, last_name, password, date_of_birth)
-            db.session.add(me)
-            db.session.commit()
-            return redirect('/login')
-    return render_template('register.html', form=form)
-
-
 @auth.route("/logout")
 def logout():
     logout_user()
