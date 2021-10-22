2036844fab2bd8f18eb74cead199ab42b6c26908
giuseppe-crea <7926745+Sylt556@users.noreply.github.com>
2021-10-22 19:16:06 +0200

    Removed useless dependency from auth.py Added a broken test case to test_auth.py

---
+++ monolith/classes/tests/test_auth.py
@@ -0,0 +1,18 @@
+import unittest
+import json
+from flask import request, jsonify
+from monolith import app
+
+class testAuth(unittest.TestCase):
+    def test_login(self):
+        tested_app = app.test_client()
+        # testing that the login page loads
+        login_reply = tested_app.get("/login")
+        self.assertEqual(login_reply.status_code, 200)
+        # test a correct login
+        response = tested_app.post(
+            '/login',
+            data=dict(email="exampl4@example.com", password="admin", login_form=""),
+            follow_redirects=True
+        )
+        self.assertIn(b'Hi Admin!', response.data)
--- monolith/views/auth.py
+++ monolith/views/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, redirect, render_template
 from flask_login import login_user, logout_user
 
 from monolith.database import User, db
-from monolith.forms import LoginForm, UserForm
+from monolith.forms import LoginForm
 
 auth = Blueprint('auth', __name__)
 
