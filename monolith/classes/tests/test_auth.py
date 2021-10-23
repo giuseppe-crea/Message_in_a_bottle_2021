42eadd86be2a9a107138e87a7b5083ad731fc234
giuseppe-crea <7926745+Sylt556@users.noreply.github.com>
2021-10-23 14:10:46 +0200

    Fixed and completed the tests for view/auth

--- monolith/classes/tests/test_auth.py
+++ monolith/classes/tests/test_auth.py
@@ -1,18 +1,51 @@
 import unittest
-import json
-from flask import request, jsonify
-from monolith import app
+from monolith.classes.tests.utils import get_testing_app
 
-class testAuth(unittest.TestCase):
-    def test_login(self):
-        tested_app = app.test_client()
-        # testing that the login page loads
-        login_reply = tested_app.get("/login")
-        self.assertEqual(login_reply.status_code, 200)
-        # test a correct login
-        response = tested_app.post(
-            '/login',
-            data=dict(email="exampl4@example.com", password="admin", login_form=""),
-            follow_redirects=True
-        )
-        self.assertIn(b'Hi Admin!', response.data)
+
+def login(client, username, password):
+    return client.post(
+                '/login',
+                data={'email': username, 'password': password},
+                follow_redirects=True
+            )
+
+
+class TestHome(unittest.TestCase):
+    def test_get_login(self):
+        tested_app = get_testing_app()
+        with tested_app:
+            rv = tested_app.get('/login')
+            assert rv.status_code == 200
+            assert b'email' in rv.data
+
+    def test_post_login(self):
+        tested_app = get_testing_app()
+        with tested_app:
+            login_reply = tested_app.get("/login")
+            self.assertEqual(login_reply.status_code, 200)
+            response = login(tested_app, 'example@example.com', 'admin')
+            self.assertIn(b'Hi Admin', response.data)
+
+    def test_bad_login(self):
+        tested_app = get_testing_app()
+        with tested_app:
+            login_reply = login(tested_app, 'bad@wrong.com', 'badpassword')
+            self.assertEqual(login_reply.status_code, 401)
+
+    def test_empty_forms_login(self):
+        tested_app = get_testing_app()
+        with tested_app:
+            login_reply = login(tested_app, '', 'badpassword')
+            assert b'This field is required.' in login_reply.data
+            login_reply = login(tested_app, 'baduser@mail.com', '')
+            assert b'This field is required.' in login_reply.data
+
+    def test_login_logout(self):
+        tested_app = get_testing_app()
+        with tested_app:
+            response = login(tested_app, 'example@example.com', 'admin')
+            self.assertIn(b'Hi Admin', response.data)
+            response = tested_app.get("/logout", follow_redirects=True)
+            assert b'Hi Anonymous' in response.data
+
+
--- monolith/classes/tests/test_home.py
+++ monolith/classes/tests/test_home.py
@@ -5,9 +5,8 @@ from monolith import app
 
 
 class TestHome(unittest.TestCase):
-    def test_my_view(self):
-        tested_app = app.test_client()
-        home_reply = tested_app.get("/")
-
-        #body = json.loads(str(home_reply.data, "utf8"))
-        return 1
+    def test_homepage(self):
+        # start by loading the home as anonymous
+        client = app.test_client()
+        rv = client.get('/')
+        assert b'Hi Anonymous' in rv.data
