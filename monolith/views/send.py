81e48b1743663f6466754031c30a73ff862414c2
giuseppe-crea <7926745+Sylt556@users.noreply.github.com>
2021-10-24 00:49:50 +0200

    Replaced the manual handling of login check in views/send.send with the built-in @login_required notation from Flask

--- monolith/views/send.py
+++ monolith/views/send.py
@@ -1,8 +1,8 @@
-import datetime
 import html
 import re
 
 from flask import Blueprint, render_template, request, escape, abort
+from flask_login import login_required
 
 from monolith.database import User, db, UnsentMessage
 from monolith.forms import SendForm
@@ -23,46 +23,44 @@ def check(email):
 
 
 @send.route('/send', methods=['POST', 'GET'])
+@login_required
 def _send():
     form = SendForm()
     correctly_sent = []
     not_correctly_sent = []
-    if current_user is not None and hasattr(current_user, 'id'):
-        if request.method == 'POST':
-            real_recipients = []
-            if form.validate_on_submit():
-                message, user_input = html.escape(form.data['message']), form.data['recipient']
-                time = form.data['time']
-                to_parse = user_input.split(', ')
-                for address in to_parse:
-                    address = address.strip()
-                    if check(address):
-                        real_recipients.append(escape(address))
-                # find users in database
-                # if user found, enqueue, otherwise print error
-                current_user_mail = getattr(current_user, 'email')
-                for address in real_recipients:
-                    exists = db.session.query(User.id).filter_by(email=address).first() is not None
-                    if exists and not address == current_user_mail:
-                        # save message, sender, receiver, date to the database of unsent messages
-                        unsent_message = UnsentMessage()
-                        unsent_message.add_message(message, current_user_mail, address,
-                                                   time.strftime("%m/%d/%Y, %H:%M:%S"))
-                        db.session.add(unsent_message)
-                        # enqueue message with celery
-                        # TODO: bring the broker online and uncomment this
-                        # deliver_message.apply_async((unsent_message.get_id(), message, current_user_mail, address),
-                        #                            eta=time)
-                        correctly_sent.append(address)
-                    else:
-                        not_correctly_sent.append(address)
-                if len(correctly_sent) > 0:
-                    db.session.commit()
-            else:
-                return render_template('error_template.html', form=form)
-            return render_template('done_sending.html', users1=correctly_sent, users2=not_correctly_sent,
-                                   text=html.unescape(message))
+    if request.method == 'POST':
+        real_recipients = []
+        if form.validate_on_submit():
+            message, user_input = html.escape(form.data['message']), form.data['recipient']
+            time = form.data['time']
+            to_parse = user_input.split(', ')
+            for address in to_parse:
+                address = address.strip()
+                if check(address):
+                    real_recipients.append(escape(address))
+            # find users in database
+            # if user found, enqueue, otherwise print error
+            current_user_mail = getattr(current_user, 'email')
+            for address in real_recipients:
+                exists = db.session.query(User.id).filter_by(email=address).first() is not None
+                if exists and not address == current_user_mail:
+                    # save message, sender, receiver, date to the database of unsent messages
+                    unsent_message = UnsentMessage()
+                    unsent_message.add_message(message, current_user_mail, address,
+                                               time.strftime("%m/%d/%Y, %H:%M:%S"))
+                    db.session.add(unsent_message)
+                    # enqueue message with celery
+                    # TODO: bring the broker online and uncomment this
+                    # deliver_message.apply_async((unsent_message.get_id(), message, current_user_mail, address),
+                    #                            eta=time)
+                    correctly_sent.append(address)
+                else:
+                    not_correctly_sent.append(address)
+            if len(correctly_sent) > 0:
+                db.session.commit()
         else:
-            return render_template('send.html', form=form)
+            return render_template('error_template.html', form=form)
+        return render_template('done_sending.html', users1=correctly_sent, users2=not_correctly_sent,
+                               text=html.unescape(message))
     else:
-        abort(401)
+        return render_template('send.html', form=form)
