e27d750d10cf221b15098c26ef1ee980985a1a9f
fraventu97 <fraventu97@hotmail.it>
2021-10-29 13:59:38 +0200

    moved old listing functionalities in list.py

--- monolith/views/send.py
+++ monolith/views/send.py
@@ -3,12 +3,11 @@ import re
 from datetime import datetime
 from math import floor
 
-from flask import Blueprint, render_template, request, escape, redirect, \
-    url_for
+from flask import Blueprint, render_template, request, escape
 from flask_login import login_required
 
 from monolith.database import User, db
-from monolith.forms import SendForm, RecipientsListForm
+from monolith.forms import SendForm
 from monolith.auth import current_user
 from monolith.background import deliver_message
 
@@ -75,35 +74,3 @@ def _send(data=""):
     else:
         return render_template('send.html', form=form)
 
-
-@send.route('/list_of_recipients', methods=['POST', 'GET'])
-def _display_users():
-
-    # instantiate the form
-    form = RecipientsListForm()
-
-    # ordering alphabetically, filtering admin accounts and the sender itself
-    _users = db.session.query(User).order_by(User.lastname).\
-        filter(User.firstname != 'Admin').all()
-
-    # sets choices
-    form.multiple_field_form.choices = \
-        [(user.email, user.lastname + ' ' + user.firstname + ': ' + user.email)
-         for user in _users]
-
-    if request.method == 'POST':  # POST request
-        if len(request.form) != 0:  # check the selection of a recipient
-            # create a list of emails, removing the submitted label
-            selected_recipient_list = \
-                request.form.getlist('multiple_field_form')
-            # create a dictionary to construct the right structure
-            payload = {'email_list': ','.join(selected_recipient_list)}
-            # send a list of comma-separated emails
-            # redirecting to /send
-            return redirect(url_for('send._send', data=payload['email_list']))
-
-        else:  # no recipient selected
-            return redirect(url_for('send._send'))
-
-    else:  # GET request, returns the list_of_recipients.html page
-        return render_template('list_of_recipients.html', form=form)
