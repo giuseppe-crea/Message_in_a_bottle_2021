bccbe9388be1fa6f06e1a7657de4677916bae45a
Francesco <gargiulo.fr@gmail.com>
2021-11-02 16:53:09 -0700

    working notification

--- monolith/views/admin.py
+++ monolith/views/admin.py
@@ -15,8 +15,8 @@ def placeholder():
 
     current_user = flask_login.current_user
     current_user_email = current_user.email
-    query = db.session.query(Notification).filter_by(user_email=\
-            current_user_email, is_read=False)
+    query = db.session.query(Notification)\
+            .filter_by(user_email=current_user_email, is_read=False)
     notifications_count = query.count()
 
     return render_template("index.html", notifications=notifications_count)
