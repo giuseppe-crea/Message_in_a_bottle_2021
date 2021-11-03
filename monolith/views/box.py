25be5e183705367d951db07ac6322189381ca404
giuseppe-crea <7926745+Sylt556@users.noreply.github.com>
2021-11-04 00:14:39 +0100

    Unified outbox and inbox in both code and template Minimized code repetition

--- monolith/views/box.py
+++ monolith/views/box.py
@@ -6,42 +6,70 @@ from sqlalchemy.exc import NoResultFound
 from monolith import send
 from monolith.database import Message
 from monolith.forms import ForwardForm
-from monolith.delete import delete_for_receiver
+from monolith.delete import remove_message
 
-inbox = Blueprint('inbox', __name__)
+box = Blueprint('box', __name__)
 
 
 # noinspection PyUnresolvedReferences
-@inbox.route("/inbox", methods=["GET"], defaults={'_id': None})
-@inbox.route("/inbox/<_id>", methods=["GET", "DELETE"])
+@box.route("/inbox", methods=["GET"], defaults={'_id': None})
+@box.route("/inbox/<_id>", methods=["GET", "DELETE"])
 @login_required
-def get_inbox(_id):
+def prep_inbox(_id):
     user_mail = current_user.get_email()
+    role = '/inbox'
+    kwargs = {'status': 2, 'receiver_email': user_mail,
+              'visible_to_receiver': True}
     if _id is not None:
+        kwargs['id'] = int(_id)
+    return get_box(kwargs, role)
+
+
+@box.route("/outbox", methods=["GET"], defaults={'_id': None})
+@box.route("/outbox/<_id>", methods=["GET", "DELETE"])
+@login_required
+def prep_outbox(_id):
+    user_mail = current_user.get_email()
+    role = '/outbox'
+    kwargs = {'status': 2, 'sender_email': user_mail,
+              'visible_to_sender': True}
+    if _id is not None:
+        kwargs['id'] = int(_id)
+    return get_box(kwargs, role)
+
+
+def get_box(kwargs, role):
+    if 'id' in kwargs:
         try:
-            message = Message().query.filter_by(
-                id=int(_id),
-                receiver_email=user_mail,
-                status=2,
-                visible_to_receiver=True
-            ).one()
+            message = Message().query.filter_by(**kwargs).one()
             if request.method == "DELETE":
-                delete_for_receiver(message)
-                return redirect('/inbox')
+                remove_message(message, role)
+                # TODO: redirect to confirmation page
+                #  this is actually overwritten by the JS currently
+                return redirect(role)
             else:
-                return render_template("list/inbox_one.html", message=message)
+                return render_template(
+                    'list/box_one.html',
+                    message=message,
+                    role=role
+                )
         except NoResultFound:
             abort(403)
     else:
-        messages = Message().query.filter_by(
-            receiver_email=user_mail,
-            status=2,
-            visible_to_receiver=True
+        messages = Message().query.filter_by(**kwargs)
+        pending_messages = None
+        if role == '/outbox':
+            kwargs['status'] = 1
+            pending_messages = Message().query.filter_by(**kwargs)
+        return render_template(
+            'list/box.html',
+            messages=messages,
+            pending=pending_messages,
+            role=role
         )
-        return render_template("list/inbox.html", messages=messages)
 
 
-@inbox.route("/inbox/forward/<_id>", methods=["GET", "POST"])
+@box.route("/inbox/forward/<_id>", methods=["GET", "POST"])
 @login_required
 def forward(_id):
     if _id is not None:
