a1601fbf7e2e55901409f43a9a1e6891b5fe6dce
giuseppe-crea <7926745+Sylt556@users.noreply.github.com>
2021-11-03 11:35:39 +0100

    Rewrote deletion for pictures Implemented celery beat in both background and docker files Implemented a garbage collection for pictures Implemented a crash recovery celery beat task that sends overdue messages

--- monolith/classes/tests/test_deletion.py
+++ monolith/classes/tests/test_deletion.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import flask
 from slugify import slugify
 
-from monolith.background import deliver_message
+from monolith.background import deliver_message, cleanup_pictures
 from monolith.database import Message
 from monolith.classes.tests.utils import get_testing_app, create_ex_users, \
     login, create_message
@@ -15,9 +15,10 @@ class TestSend(unittest.TestCase):
     def test_correct_removal(self):
         tested_app = get_testing_app()
         with tested_app:
-            users = create_ex_users(tested_app, 2)
+            users = create_ex_users(tested_app, 3)
             user1, password1 = users[0]
             user2, password2 = users[1]
+            user3, password3 = users[2]
             # this test uses the upload function to showcase how
             # unused pics are removed from the server
             lenna_src = './monolith/static/images/Lenna.png'
@@ -35,7 +36,7 @@ class TestSend(unittest.TestCase):
                 '/send',
                 data={
                     'message': "Short test message",
-                    'recipient': user2,
+                    'recipient': user2+', '+user3,
                     'time': "2199-01-01T01:01",
                     'file': file
                 },
@@ -46,8 +47,9 @@ class TestSend(unittest.TestCase):
             # check that the message has been added to the database
             message = Message().query.filter_by(id=int(1))
             assert message is not None
-            # deliver the message
+            # deliver the messages
             deliver_message(flask.current_app, 1)
+            deliver_message(flask.current_app, 2)
             # assert that the new pic has been saved
             assert os.path.isfile(lenna_dst)
             # try and view the message in my inbox
@@ -72,7 +74,34 @@ class TestSend(unittest.TestCase):
             # check that the message has been deleted from the database
             message = Message().query.filter_by(id=int(1)).first()
             assert message is None
-            # check that the received pic has been deleted
+            # call pic cleanup service
+            cleanup_pictures(flask.current_app)
+            # check that the received pic hasn't been deleted
+            assert os.path.isfile(lenna_dst)
+            tested_app.get('/logout')
+            # now delete it for user3 too
+            rv = login(tested_app, user3, password3)
+            assert rv.status_code == 200
+            rv = tested_app.get('/inbox_delete/2', follow_redirects=True)
+            assert rv.status_code == 200
+            assert bytes(user1, 'utf-8') not in rv.data
+            # check that the message hasn't been deleted from the database
+            message = Message().query.filter_by(id=int(2)).first()
+            assert message is not None
+            # call pic cleanup service
+            cleanup_pictures(flask.current_app)
+            # check that the received pic hasn't been deleted
+            assert os.path.isfile(lenna_dst)
+            tested_app.get('/logout')
+            # finally, log back as user1, delete message 2
+            rv = login(tested_app, user1, password1)
+            assert rv.status_code == 200
+            rv = tested_app.get('/outbox_delete/2', follow_redirects=True)
+            assert rv.status_code == 200
+            assert bytes(user3, 'utf-8') not in rv.data
+            # call pic cleanup service
+            cleanup_pictures(flask.current_app)
+            # check that the received pic hasn't been deleted
             assert not os.path.isfile(lenna_dst)
             # cleanup the empty user folder
             os.rmdir(Path(lenna_dst).parent)
--- monolith/delete.py
+++ monolith/delete.py
@@ -1,5 +1,3 @@
-import os
-
 from monolith.database import db, Message
 
 
@@ -7,7 +5,6 @@ from monolith.database import db, Message
 def delete_for_receiver(message_query: Message):
     message_query.visible_to_receiver = False
     if not message_query.visible_to_sender:
-        delete_picture(message_query)
         db.session.delete(message_query)
     db.session.commit()
 
@@ -15,13 +12,5 @@ def delete_for_receiver(message_query: Message):
 def delete_for_sender(message_query: Message):
     message_query.visible_to_sender = False
     if not message_query.visible_to_receiver:
-        delete_picture(message_query)
         db.session.delete(message_query)
     db.session.commit()
-
-
-def delete_picture(message_query):
-    if message_query.image is not None and message_query.image != '':
-        file_path = os.path.join('./monolith/static', message_query.image)
-        if os.path.exists(file_path):
-            os.remove(file_path)
