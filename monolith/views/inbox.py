ba4a8f5a8d6730868cd433c1ac19549e75ccaf26
giuseppe-crea <7926745+Sylt556@users.noreply.github.com>
2021-11-03 16:30:10 +0100

    Moved deletion within inbox/<_id> route, using ajax to issue a delete request from within the html

--- monolith/views/inbox.py
+++ monolith/views/inbox.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, redirect
+from flask import Blueprint, abort, redirect, request
 from flask.templating import render_template
 from flask_login import login_required, current_user
 from sqlalchemy.exc import NoResultFound
@@ -13,7 +13,7 @@ inbox = Blueprint('inbox', __name__)
 
 # noinspection PyUnresolvedReferences
 @inbox.route("/inbox", methods=["GET"], defaults={'_id': None})
-@inbox.route("/inbox/<_id>", methods=["GET"])
+@inbox.route("/inbox/<_id>", methods=["GET", "DELETE"])
 @login_required
 def get_inbox(_id):
     user_mail = current_user.get_email()
@@ -25,7 +25,11 @@ def get_inbox(_id):
                 status=2,
                 visible_to_receiver=True
             ).one()
-            return render_template("list/inbox_one.html", message=message)
+            if request.method == "DELETE":
+                delete_for_receiver(message)
+                return redirect('/inbox')
+            else:
+                return render_template("list/inbox_one.html", message=message)
         except NoResultFound:
             abort(403)
     else:
@@ -35,26 +39,6 @@ def get_inbox(_id):
             visible_to_receiver=True
         )
         return render_template("list/inbox.html", messages=messages)
-
-
-@inbox.route("/inbox_delete/<_id>", methods=["GET"])
-@login_required
-def delete(_id):
-    user_mail = current_user.get_email()
-    if _id is not None:
-        try:
-            message = Message().query.filter_by(
-                id=int(_id),
-                receiver_email=user_mail,
-                status=2,
-                visible_to_receiver=True
-            ).one()
-            # TODO: add confirmation window maybe?
-            #  add a successful deletion notification?
-            delete_for_receiver(message)
-            return redirect('/inbox')
-        except NoResultFound:
-            abort(403)
 
 
 @inbox.route("/inbox/forward/<_id>", methods=["GET", "POST"])
--- monolith/views/outbox.py
+++ monolith/views/outbox.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, redirect
+from flask import Blueprint, abort, redirect, request
 from flask.templating import render_template
 from flask_login import login_required, current_user
 from sqlalchemy.exc import NoResultFound
@@ -10,7 +10,7 @@ outbox = Blueprint('outbox', __name__)
 
 
 @outbox.route("/outbox", methods=["GET"], defaults={'_id': None})
-@outbox.route("/outbox/<_id>", methods=["GET"])
+@outbox.route("/outbox/<_id>", methods=["GET", "DELETE"])
 @login_required
 def get_outbox(_id):
     user_mail = current_user.get_email()
@@ -38,26 +38,10 @@ def get_outbox(_id):
                 sender_email=user_mail
             ).one()
             # noinspection PyUnresolvedReferences
-            return render_template("list/outbox_one.html", message=message)
+            if request.method == "DELETE":
+                delete_for_sender(message)
+                return redirect('/outbox')
+            else:
+                return render_template("list/outbox_one.html", message=message)
         except NoResultFound:
             abort(403)
-
-
-@outbox.route("/outbox_delete/<_id>", methods=["GET"])
-@login_required
-def delete(_id):
-    user_mail = current_user.get_email()
-    if _id is not None:
-        try:
-            message = Message().query.filter_by(
-                id=int(_id),
-                sender_email=user_mail,
-                visible_to_sender=True
-            ).one()
-            # TODO: add confirmation window maybe?
-            #  add a successful deletion notification?
-            delete_for_sender(message)
-            return redirect('/outbox')
-        except NoResultFound:
-            abort(403)
-    abort(404)
