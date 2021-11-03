535645daf3d0c263fe328f9a78345d84fe85cb34
Francesco <gargiulo.fr@gmail.com>
2021-11-03 03:24:44 -0700

    working send notifications

--- monolith/background.py
+++ monolith/background.py
@@ -49,16 +49,16 @@ def deliver_message(app, message_id):
             message.status = 2
             # notify recipient
             notification = Notification()
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            timestamp = message.time
             title = message.sender_email + " Sent You a Message"
             description = "Check Your Inbox to Open It"
             notification.add_notification(
                 message.receiver_email,
-                        title,
-                        description,
-                        timestamp,
-                        False
-                        )
+                title,
+                description,
+                timestamp,
+                False
+                )
             db.session.add(notification)
             db.session.commit()
         except NoResultFound:
